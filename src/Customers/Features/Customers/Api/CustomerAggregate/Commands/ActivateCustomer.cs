ec54880fea3bcff2599affde980f4297cd108ab2
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-08 12:47:51 +0100

    feat: add ActivateCustomer slice with unit tests

---
+++ src/Customers/Features/Customers/Api/CustomerAggregate/Commands/ActivateCustomer.cs
@@ -0,0 +1,41 @@
+namespace Customers.Features.Customers.Api.CustomerAggregate.Commands;
+
+public class ActivateCustomer : IFeatureModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/customer/activate", Handler);
+    }
+
+    public static Func<IService, Task<IResult>> Handler => async (service) =>
+    {
+        var response = await service.HandleAsync();
+        return Results.Ok(response);
+    };
+
+    public interface IService
+    {
+        Task<CustomerResponse> HandleAsync();
+    }
+
+    [Injectable]
+    public class Service(
+        Customer.Activate activateCustomer,
+        Guid tenantId,
+        IRepository repository,
+        IUnitOfWork unitOfWork) : IService
+    {
+        public async Task<CustomerResponse> HandleAsync()
+        {
+            var customer = await repository.Get(tenantId);
+
+            activateCustomer.Execute(customer);
+
+            await unitOfWork.SaveChangesAsync();
+
+            return CustomerResponse.Map(customer);
+        }
+    }
+
+    public interface IRepository : IUpdate<Customer, Guid> { }
+}
