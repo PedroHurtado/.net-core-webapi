c8b3e7f82c29d00a9575e46da46893d7a36a33e6
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-09 20:47:20 +0100

    feat: configure AuthDbContext with User entity mapping
    Add DbSet<User>, ignore computed properties (IsOAuth, HasPassword),
    configure Password as ComplexProperty for Firestore persistence.

--- src/Auth/Infrastructure/AuthDbContext.cs
+++ src/Auth/Infrastructure/AuthDbContext.cs
@@ -3,8 +3,21 @@ namespace Auth.Infrastructure;
 public class AuthDbContext(DbContextOptions<AuthDbContext> options) :
     DbContext(options), IEntityLookup, IQuery, IChangeTracker, IUnitOfWork
 {
+    public DbSet<User> Users => Set<User>();
+
     public IQueryable<T> Query<T>() where T : class, IEntity
     {
         return Set<T>().AsQueryable().AsNoTracking();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Ignore(u => u.IsOAuth);
+            entity.Ignore(u => u.HasPassword);
+
+            entity.ComplexProperty(u => u.Password);
+        });
     }
 }
