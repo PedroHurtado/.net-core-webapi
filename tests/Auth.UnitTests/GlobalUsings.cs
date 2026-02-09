cb9c800b9bb8c43f71f18d4a756971097f26c976
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-09 18:03:10 +0100

    chore: add NuGet packages and global usings for Google OAuth infrastructure

--- tests/Auth.UnitTests/GlobalUsings.cs
+++ tests/Auth.UnitTests/GlobalUsings.cs
@@ -9,7 +9,13 @@ global using Fudie.Domain;
 global using Fudie.DependencyInjection;
 global using Fudie.Infrastructure;
 
+global using System.Net;
+global using System.Security.Cryptography;
+global using System.Security.Cryptography.X509Certificates;
+global using System.Text.Json;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.IdentityModel.JsonWebTokens;
+global using Microsoft.IdentityModel.Tokens;
 
 global using Auth.Infrastructure;
 global using Auth.Infrastructure.Google;
