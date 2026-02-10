79ce5eb9df72f2728a77bd6b548ae662d6bc6e73
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-10 19:45:56 +0100

    feat:add TenantRole.Create command and tests

---
+++ src/Auth/Features/Roles/Domain/TenantRoleAggregate/Commands/TenantRole/TenantRole_Create.cs
@@ -0,0 +1,31 @@
+namespace Auth.Features.Roles.Domain.TenantRoleAggregate;
+
+public record CreateTenantRoleCommand(
+    Guid TenantId,
+    string Name,
+    string Description
+);
+
+public partial class TenantRole
+{
+    [Injectable(ServiceLifetime.Singleton)]
+    public class Create(
+        IValidator<TenantRole> tenantRoleValidator
+    ) : AbstractCreateCommand<CreateTenantRoleCommand, TenantRole>
+    {
+        public override TenantRole Execute(CreateTenantRoleCommand command)
+        {
+            var role = new TenantRole(Guid.NewGuid())
+            {
+                TenantId = command.TenantId,
+                Name = command.Name,
+                Description = command.Description,
+                IsSystem = false,
+                IsEditable = true,
+                IsDeletable = true
+            };
+
+            return tenantRoleValidator.ValidateOrThrow(role);
+        }
+    }
+}
