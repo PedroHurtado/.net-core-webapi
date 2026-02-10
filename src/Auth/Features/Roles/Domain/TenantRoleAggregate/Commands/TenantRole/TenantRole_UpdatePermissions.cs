eb754f14dda3904d4a4467b33996e1fef94ea4d3
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-10 19:47:17 +0100

    feat:add TenantRole.UpdatePermissions command and tests

---
+++ src/Auth/Features/Roles/Domain/TenantRoleAggregate/Commands/TenantRole/TenantRole_UpdatePermissions.cs
@@ -0,0 +1,27 @@
+namespace Auth.Features.Roles.Domain.TenantRoleAggregate;
+
+public record UpdatePermissionsCommand(
+    List<string> Groups,
+    List<string> AdditionalScopes,
+    List<string> ExcludedScopes
+);
+
+public partial class TenantRole
+{
+    [Injectable(ServiceLifetime.Singleton)]
+    public class UpdatePermissions(
+        IValidator<TenantRole> tenantRoleValidator
+    ) : AbstractModifyCommand<UpdatePermissionsCommand, TenantRole>
+    {
+        public override TenantRole Execute(TenantRole role, UpdatePermissionsCommand command)
+        {
+            ConflictGuard.ThrowIf(!role.IsEditable, "This role cannot be edited");
+
+            role._groups = [.. command.Groups];
+            role._additionalScopes = [.. command.AdditionalScopes];
+            role._excludedScopes = [.. command.ExcludedScopes];
+
+            return tenantRoleValidator.ValidateOrThrow(role);
+        }
+    }
+}
