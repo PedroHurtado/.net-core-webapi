233d3c4a82e79b78c1712d10755251e7912ad854
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-10 23:20:41 +0100

    feat:add GetTenantRole query and unit tests

---
+++ src/Auth/Features/Roles/Api/TenantRoleAggregate/Queries/GetTenantRole.cs
@@ -0,0 +1,34 @@
+namespace Auth.Features.Roles.Api.TenantRoleAggregate.Queries;
+
+public class GetTenantRole : IFeatureModule
+{
+    public static Func<IService, Guid, Task<IResult>> Handler =>
+        async (service, id) =>
+        {
+            var response = await service.HandleAsync(id);
+            return Results.Ok(response);
+        };
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/tenant-roles/{id}", Handler);
+    }
+
+    public interface IService
+    {
+        Task<TenantRoleResponse> HandleAsync(Guid id);
+    }
+
+    [Injectable]
+    public class Service(IRepository repository) : IService
+    {
+        public async Task<TenantRoleResponse> HandleAsync(Guid id)
+        {
+            var role = await repository.Get(id);
+            return TenantRoleResponse.Map(role);
+        }
+    }
+
+    [AsNoTracking]
+    public interface IRepository : IGet<TenantRole, Guid> { }
+}
