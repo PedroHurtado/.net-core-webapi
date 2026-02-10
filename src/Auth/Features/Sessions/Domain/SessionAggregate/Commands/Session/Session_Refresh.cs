6c49a3df12a6b4c0b9c4c2a2ddb54ab14aad0a63
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-10 10:46:27 +0100

    feat:add Session.Refresh command with unit tests

---
+++ src/Auth/Features/Sessions/Domain/SessionAggregate/Commands/Session/Session_Refresh.cs
@@ -0,0 +1,21 @@
+namespace Auth.Features.Sessions.Domain.SessionAggregate;
+
+public partial class Session
+{
+    [Injectable(ServiceLifetime.Singleton)]
+    public class Refresh(
+        IValidator<Session> sessionValidator
+    ) : AbstractModifyCommand<Session>
+    {
+        public override Session Execute(Session session)
+        {
+            UnauthorizedGuard.ThrowIf(session.IsExpired, "Session expired");
+
+            var now = DateTimeOffset.UtcNow;
+            session.LastActivityAt = now;
+            session.ExpiresAt = now.AddDays(30);
+
+            return sessionValidator.ValidateOrThrow(session);
+        }
+    }
+}
