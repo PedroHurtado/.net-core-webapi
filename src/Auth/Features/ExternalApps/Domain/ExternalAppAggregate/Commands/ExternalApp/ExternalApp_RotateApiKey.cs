846c64e0fc906bf84077ebbb88b4822c0cf11c28
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-11 15:30:34 +0100

    feat:add ExternalApp.RotateApiKey command with tests

---
+++ src/Auth/Features/ExternalApps/Domain/ExternalAppAggregate/Commands/ExternalApp/ExternalApp_RotateApiKey.cs
@@ -0,0 +1,31 @@
+namespace Auth.Features.ExternalApps.Domain.ExternalAppAggregate;
+
+public record RotateExternalAppApiKeyCommand(
+    string ApiKeyHash,
+    string ApiKeyPrefix
+);
+
+public partial class ExternalApp
+{
+    [Injectable(ServiceLifetime.Singleton)]
+    public class RotateApiKey(
+        IValidator<ExternalApp> externalAppValidator
+    ) : AbstractModifyCommand<RotateExternalAppApiKeyCommand, ExternalApp>
+    {
+        public override ExternalApp Execute(ExternalApp externalApp, RotateExternalAppApiKeyCommand command)
+        {
+            ConflictGuard.ThrowIf(
+                externalApp.InvitationStatus != InvitationStatus.Accepted,
+                "External app invitation has not been accepted");
+
+            ConflictGuard.ThrowIf(
+                !externalApp.IsActive,
+                "Cannot rotate key for inactive external app");
+
+            externalApp.ApiKeyHash = command.ApiKeyHash;
+            externalApp.ApiKeyPrefix = command.ApiKeyPrefix;
+
+            return externalAppValidator.ValidateOrThrow(externalApp);
+        }
+    }
+}
---
+++ tests/Auth.UnitTests/Features/ExternalApps/Domain/ExternalAppAggregateTests/Commands/ExternalAppTests/ExternalAppRotateApiKeyTests.cs
@@ -0,0 +1,62 @@
+namespace Auth.UnitTests.Features.ExternalApps.Domain.ExternalAppAggregateTests.Commands.ExternalAppTests;
+
+public class ExternalAppRotateApiKeyTests(DomainFixture fixture) : IClassFixture<DomainFixture>
+{
+    private readonly ExternalApp.RotateApiKey _rotateApiKey = fixture.Get<ExternalApp.RotateApiKey>();
+
+    [Fact]
+    public void Execute_WithAcceptedAndActive_RotatesApiKey()
+    {
+        var externalApp = new TestableExternalApp(Guid.NewGuid())
+            .WithTenantId(Guid.NewGuid())
+            .WithName("TPV MiSoftware")
+            .WithInvitationEmail("dev@misoftware.com")
+            .WithInvitationStatus(InvitationStatus.Accepted)
+            .WithIsActive(true)
+            .WithApiKeyHash("oldHash")
+            .WithApiKeyPrefix("fud_old1");
+
+        var command = new RotateExternalAppApiKeyCommand("newHash", "fud_new1");
+
+        var result = _rotateApiKey.Execute(externalApp, command);
+
+        result.ApiKeyHash.Should().Be("newHash");
+        result.ApiKeyPrefix.Should().Be("fud_new1");
+    }
+
+    [Fact]
+    public void Execute_WhenNotAccepted_ThrowsConflictException()
+    {
+        var externalApp = new TestableExternalApp(Guid.NewGuid())
+            .WithTenantId(Guid.NewGuid())
+            .WithName("TPV MiSoftware")
+            .WithInvitationEmail("dev@misoftware.com")
+            .WithInvitationStatus(InvitationStatus.Pending)
+            .WithIsActive(true);
+
+        var command = new RotateExternalAppApiKeyCommand("hash", "fud_pre1");
+
+        var act = () => _rotateApiKey.Execute(externalApp, command);
+
+        act.Should().Throw<ConflictException>()
+            .WithMessage("*External app invitation has not been accepted*");
+    }
+
+    [Fact]
+    public void Execute_WhenInactive_ThrowsConflictException()
+    {
+        var externalApp = new TestableExternalApp(Guid.NewGuid())
+            .WithTenantId(Guid.NewGuid())
+            .WithName("TPV MiSoftware")
+            .WithInvitationEmail("dev@misoftware.com")
+            .WithInvitationStatus(InvitationStatus.Accepted)
+            .WithIsActive(false);
+
+        var command = new RotateExternalAppApiKeyCommand("hash", "fud_pre1");
+
+        var act = () => _rotateApiKey.Execute(externalApp, command);
+
+        act.Should().Throw<ConflictException>()
+            .WithMessage("*Cannot rotate key for inactive external app*");
+    }
+}
