b02b131404e0d8fc1740cfd259273a8871e605ea
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-12 18:55:34 +0100

    feat:add Subscriptions microservice with Firestore, gateway routing and docker-compose integration

---
+++ src/Subscriptions/GlobalUsings.cs
@@ -0,0 +1,25 @@
+#pragma warning disable IDE0005
+
+global using System.ComponentModel;
+global using System.Globalization;
+global using System.Reflection;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+
+global using FluentValidation;
+global using Fudie.Domain;
+global using Fudie.DependencyInjection;
+global using Fudie.Features;
+global using Fudie.Http;
+global using Fudie.Infrastructure;
+global using Fudie.Validation;
+global using Microsoft.AspNetCore.StaticFiles;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.FileProviders;
+
+global using Fudie.Firestore.EntityFrameworkCore.Infrastructure;
+global using Fudie.Firestore.EntityFrameworkCore.Metadata.Builders;
+
+global using Subscriptions.Infrastructure;
