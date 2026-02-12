70ac2b4425182b0eb564b6cf9ba51c0edd962d2d
Pedro Hurtado <pedrohurt@gmail.com>
2026-02-12 19:52:59 +0100

    feat:add Subscriptions microservice and unit test project structure

---
+++ tests/Subscriptions.UnitTests/GlobalUsings.cs
@@ -0,0 +1,21 @@
+global using FluentAssertions;
+global using FluentValidation;
+global using Moq;
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Query;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Linq.Expressions;
+
+global using Fudie.Domain;
+global using Fudie.DependencyInjection;
+global using Fudie.Infrastructure;
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Http.HttpResults;
+
+global using Subscriptions.Infrastructure;
+
+// global using Subscriptions.UnitTests.Helpers;
+
+global using Fudie.Tests.Common;
