acd7ce3acbdbc96accf77c48abc464186f1342bc
Pedro Hurtado <pedrohurt@gmail.com>
2026-03-03 19:56:25 +0100

    refactor(auth, pubsub-gcp): migrate from Fudie ProjectReference to NuGet package v1.0.4

--- src/Fudie.PubSub.Gcp/GlobalUsings.cs
+++ src/Fudie.PubSub.Gcp/GlobalUsings.cs
@@ -1,5 +1,2 @@
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
-global using Fudie.PubSub.Transport;
-global using Fudie.PubSub.Serialization;
-global using Fudie.PubSub.Hosting;
